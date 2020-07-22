7ba5ea7c2a30d8a09bea4b9e818ddba21c89270f
Neil Chuang <karta134033@gmail.com>
2020-07-22 17:22:03 +0800

    2020/07/22 新增: 1.Combat事件 2.判斷點擊是否超出邊界的機制

---
+++ Assets/Script/Combat/Fighter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using RPG.Combat;
+
+namespace RPG.Fight
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack(CombatTarget target)
+        {
+            print("Take that");
+        }
+    }
+}
--- Assets/Script/Control/PlayerController.cs
+++ Assets/Script/Control/PlayerController.cs
@@ -1,23 +1,43 @@
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using RPG.Fight;
 
 namespace RPG.Control {
-  public class PlayerController : MonoBehaviour {
+    public class PlayerController : MonoBehaviour {
 
-      void Update() {
-          if (Input.GetMouseButtonDown(0)) {
-              MoveToCursor();
-          }
-      }
+        void Update() {
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
+            print("hasn't hit");
+        }
 
-      private void MoveToCursor() {
-          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-          RaycastHit hit;
-          bool hasHit = Physics.Raycast(ray, out hit);
-          if (hasHit) {
-              GetComponent<Mover>().MoveTo(hit.point);
-          }
+        public bool InteractWithCombat() {
+            RaycastHit[] hits = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition));
+            foreach (RaycastHit hit in hits) {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
 
-      }
-  }
+                if (Input.GetMouseButtonDown(0)) {
+                    print(target);
+                    GetComponent<Fighter>().Attack(target);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool InteractWithMovement() {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit) {
+                if (Input.GetMouseButtonDown(0)) {
+                    GetComponent<Mover>().MoveTo(hit.point);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
 }
