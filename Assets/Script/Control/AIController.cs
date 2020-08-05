57e141c4b68a90f0fc365e105455a517bc7b6c34
Neil Chuang <karta134033@gmail.com>
2020-08-05 13:56:04 +0800

    2020/08/05 新增: 1.Gizmos畫線 2.AI守護的位置

--- Assets/Script/Control/AIController.cs
+++ Assets/Script/Control/AIController.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Combat;
+using RPG.Movement;
 
 namespace RPG.Control {
     public class AIController : MonoBehaviour {
         float chaseDistance = 5f;
         Fighter fighter;
         Health health;
+        Mover mover;
         GameObject player;
+        Vector3 guardPosition;
         private void Start() {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            guardPosition = transform.position;
             player = GameObject.FindWithTag("Player");
         }
 
@@ -21,15 +26,22 @@ namespace RPG.Control {
                 return;
             }
             if (InAttackRange() && fighter.CanAttack(player)) {
-                GetComponent<Fighter>().Attack(player);
+                fighter.Attack(player);
             } 
             else {
                 fighter.Cancel();
+                mover.MoveTo(guardPosition);
             }
         }
 
         private bool InAttackRange() {
             return Vector3.Distance(player.transform.position, transform.position) < chaseDistance;
+        }
+
+        // Called by Unity
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 }
